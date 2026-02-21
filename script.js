ab22b3294b2701fedc500c78835ed7e54bb3f3f7
profedney <prof.edneyrossi@gmail.com>
2026-02-21 12:11:21 -0300

    add slide show photos

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ function toggleMenu() {
     menu.className = menu.className.replace(" w3-show", "");
   }
 }
+
+// Slideshow automático (W3.CSS style)
+var slideIndex = 0;
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 4000); // 4 segundos
+}
+
+// Inicia após carregar a página
+document.addEventListener("DOMContentLoaded", showSlides);
+
